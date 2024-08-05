39d33c99782f9325ba874b37aeffacaa1f1b9dea
MounikaAmsha <mounika.mahalakshmi@gmail.com>
2024-08-05 09:27:28 +0530

    Third Commit

--- src/Demoproject/Test2.java
+++ src/Demoproject/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Monkey Husband");
-System.out.println("Monkey Husband Fuck u");
+System.out.println("Monkey Husband Love you");
 	}
 
 }
